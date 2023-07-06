fb7f97ffc98072b5938f42aa1753ff80808fa7a7
Stefan Wiedemann <wistefan@googlemail.com>
2023-07-06 15:59:59 +0200

    and again

--- generate.sh
+++ generate.sh
@@ -24,6 +24,7 @@ for dir in $dscChartFolder/charts/*/; do
     echo $name: >>  $dscChartFolder/values.yaml
     cat $valuesFile | sed 's/^/  /' >>  $dscChartFolder/values.yaml
 
+    helm dependency build ${dir}
 done
 # fix values in the chart yaml
 version="${1:=0.0.0}"
