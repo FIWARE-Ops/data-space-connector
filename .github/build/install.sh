e59cb6dc3df9536fbb787708b9af1c0823abf1b4
Dennis Wendland <dennis.wendland@fiware.org>
2024-07-04 15:42:48 +0200

    Increase helm version

--- .github/build/install.sh
+++ .github/build/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget "https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz"
-tar zxf helm-v3.4.1-linux-amd64.tar.gz
+wget "https://get.helm.sh/helm-v3.15.2-linux-amd64.tar.gz"
+tar zxf helm-v3.15.2-linux-amd64.tar.gz
 mkdir bin
 mv linux-amd64/helm ./bin/helm
 
