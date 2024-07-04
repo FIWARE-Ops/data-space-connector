16107eac05ede6c89335082f386b86534dbf8d51
Dennis Wendland <dennis.wendland@fiware.org>
2024-07-04 15:54:21 +0200

    Proper error handling in eval.sh

--- .github/scripts/eval.sh
+++ .github/scripts/eval.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
 CHARTS=$(pwd)/charts/*
+RETURN_VAL=0
 for chart in $CHARTS
 do
  ./bin/helm dependency build ${chart}
  ./bin/helm template ${chart} | kubeconform -strict
+
+ ret=$?
+ if [ $ret -ne 0 ]; then
+     RETURN_VAL=$ret
+ fi
 done
+
+exit $RETURN_VAL
